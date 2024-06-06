d71a1546a749d099e331c04a83c2725c28d6c579
ISAP OU <ceo@isap.me>
2024-06-06 21:53:12 +0200

    Revert

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Traits\Cacheable;
+use Artesaos\SEOTools\Facades\SEOTools;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+use function __;
+
+class PageController extends Controller
+{
+    use Cacheable;
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request, string $slug = '/')
+    {
+        /** @var Post $post */
+        $post = Cache::remember(
+            $this->getCacheKey('post', $slug, LaravelLocalization::getCurrentLocale()),
+            $this->getCacheTtl(),
+            fn () => Post::findBySlug($slug)
+        );
+
+        $this->sharedData();
+
+        if (empty($post)) {
+            return $this->notFound($request);
+        }
+
+        SEOTools::setTitle($post->getSEOTitle());
+        SEOTools::setDescription($post->getSEODescription());
+        SEOTools::jsonLd()->addImage($post->getSEOImageUrl());
+        SEOTools::opengraph()->addImage($post->getSEOImageUrl());
+
+        return view('pages.' . $post->page_type, [
+            'post' => $post,
+        ]);
+    }
+
+    // TODO: make cache for performance
+    protected function sharedData(): void
+    {
+        // Implement View::share if needed
+    }
+
+    protected function notFound(
+        Request $request
+    ): \Illuminate\Http\Response {
+        SEOTools::setTitle(__('Page not found'));
+
+        return response()->view('pages.errors.404')->setStatusCode(404);
+    }
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-
+    {!! SEO::generate() !!}
     @if(app()->hasDebugModeEnabled())
         <meta name="robots" content="noindex">
     @endif
-
     @if(file_exists(resource_path('views/favicons.blade.php')))
         @include('favicons')
     @endif
@@ -19,7 +18,7 @@
     @endif
 </head>
 <body class="font-sans antialiased">
-
+@yield('content')
 @vite(['resources/js/app.js'])
 </body>
 </html>
